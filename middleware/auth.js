f9250ef82126becddb3521e1dffa09f81d6d4986
Alice Ndege <alicedeborah45@gmail.com>
2025-02-21 04:00:49 +0300

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,7 +1,7 @@
-// middleware/auth.js - Authentication Middleware for Hotel Management System
+//  Authentication Middleware for Hotel Management System
 module.exports.authenticateUser = (allowedRoles) => {
     return (req, res, next) => {
-        // Check if user is logged in and if their role is among the allowed roles
+        
         if (!req.session.user || !allowedRoles.includes(req.session.user.role)) {
             return res.status(403).send('Access denied. You do not have permission to view this page.');
         }
